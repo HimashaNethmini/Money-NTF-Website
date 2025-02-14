36e0b3acb781cebdef533901977434c7e190a5fa
HimashaNethmini <himashanethmini05@gmail.com>
2025-02-14 23:26:49 +0530

    feat: added styling to page

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,16 +1,18 @@
 import "./globals.css";
-import {Inria_Sans} from 'next/font/google'
+import { Inria_Sans } from "next/font/google";
 import type { Metadata } from "next";
+import { relative } from "path";
 
 const inriaSans = Inria_Sans({
-  variable: '--font-inria-sans',
-  subsets: ['latin'],
-  weights:['400', '700', '300']
-})
+  variable: "--font-inria-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
 
-export const metadata : Metadata= {
+export const metadata: Metadata = {
   title: "Money",
-  description: "Real time NTF website to increase money"}
+  description: "Real time NTF website to increase money",
+};
 
 export default function RootLayout({
   children,
@@ -19,8 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body 
-      className= {`${inriaSans.variable}`}>
+      <body className={`${inriaSans.variable} relative mx-auto box-border max-w-[1728px] px-[17px] py-[10px] antialiased`}>
         {children}
       </body>
     </html>
