cf5e07be10192de06815d9b8f08f9e345acc4a43
HimashaNethmini <himashanethmini05@gmail.com>
2025-02-26 19:07:31 +0530

    make the header

--- my-app/app/_components/header/header.tsx
+++ my-app/app/_components/header/header.tsx
@@ -1,12 +1,15 @@
+"use client";
 import Logo from './logo'
 import MenuIcon from './menuIcon'
+import React, { useState } from 'react'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <header>
         <div className="flex justify-between items-center">
             <Logo />
-            <MenuIcon />
+            <MenuIcon isOpen={isOpen} setIsOpen={setIsOpen}/>
         </div>
 
     </header>
--- my-app/app/_components/header/menuIcon.tsx
+++ my-app/app/_components/header/menuIcon.tsx
@@ -1,19 +1,41 @@
-import React from 'react'
+"use client";
 
-const menuIcon = () => {
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+const menuIcon = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+}) => {
   return (
-    <div>
-      <div 
+    <div
       data-menu-icon
-      className="flex flex-col gap-[4px] cursor-pointer relative z-[51]">
-        <div className="w-[16.5px] h-[1.5px] bg-white transition-all duration-300"></div>
-        <div className="w-[16.5px] h-[1.5px] bg-white transition-all duration-300"></div>
-        <div className="w-[16.5px] h-[1.5px] bg-white transition-all duration-300"></div>
-
-      </div>
-
+      className="flex flex-col gap-[4px] cursor-pointer relative z-[51]"
+      onClick ={() => setIsOpen(!isOpen)}
+    >
+      <div
+        className={twMerge(
+          "w-[16.5px] h-[1.5px] bg-white transition-all duration-300",
+          isOpen && "rotate-45 translate-y-[6px]"
+        )}
+      ></div>
+      <div
+        className={twMerge(
+          "w-[16.5px] h-[1.5px] bg-white transition-all duration-300",
+          isOpen && "opacity-0"
+        )}
+      ></div>
+      <div
+        className={twMerge(
+          "w-[16.5px] h-[1.5px] bg-white transition-all duration-300",
+          isOpen && "-rotate-45 -translate-y-[6px]"
+        )}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
-export default menuIcon
+export default menuIcon;
