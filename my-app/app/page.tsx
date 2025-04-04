2c51daf93c9b9ffe8083e6cea635063184b1c836
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-04 22:45:23 +0530

    added HeroSection

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,9 @@
+import HeroSection from "./_components/hero-section/hero-section";
+
 export default function Home() {
   return (
-    <div >
+    <div className="flex flex-col gap-[40px] pt-[40px] lg:gap-[145px] lg:py-[11px]">
+      <HeroSection />
     </div>
   );
 }
