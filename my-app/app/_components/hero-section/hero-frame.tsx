d6bf8252f9a86a6f94599c0e8dcad571df26ab8e
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-05 22:18:46 +0530

    added animation - hero section

---
+++ my-app/app/_components/hero-section/hero-frame.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "./hero-section.module.css";
+
+//define animations
+const animations = {
+    scaleDown: {
+        initial: { backgroundSize: "150%" },
+        animate: { backgroundSize: "100%" },
+        transition: { duration: 3.5, ease: "easeInOut" },
+    },
+    move: {
+        initial: { backgroundSize: "100% center" },
+        animate: { backgroundSize: "50% center" },
+        transition: { duration: 3, ease: "easeInOut" },
+    },
+}
+const HeroFrame = ({
+  height,
+  width,
+  bgImage,
+  animationType = "scaleDown",
+}: {
+  height: {
+    base: number;
+    lg: number;
+  };
+  width: {
+    base: number;
+    lg: number;
+  };
+  bgImage: string;
+  animationType?: "scaleDown" | "move";
+}) => {
+    const animation = animations[animationType] || animations.scaleDown;
+  return (
+    <motion.div
+    className={styles.frame}
+      style={
+        {
+          backgroundImage: `url(${bgImage})`,
+          height: `${height.base}px`,
+          width: `${width.base}px`,
+          "--lg-width": `${width.lg}px`,
+          "--lg-height": `${height.lg}px`,
+        } as React.CSSProperties
+      }
+
+      initial={animation.initial}
+      animate={animation.animate}
+      transition={animation.transition}
+    />
+  );
+};
+
+export default HeroFrame;
--- my-app/app/_components/hero-section/hero-section.tsx
+++ my-app/app/_components/hero-section/hero-section.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import HeroFrame from "./hero-frame";
 
 const HeroSection = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col gap-[38x] lg:gap-[16px] lg:py-[11px]">
+      {/* slogan */}
+      <div className="text-center px-[10px]">
+        Money NTF is a platform that allows you to create, buy, and sell NFTs
+        with ease.
+      </div>
 
-export default HeroSection
+      {/* hero title */}
+      <div className="text-25 flex flex-col items-center justify-center gap-[14px] font-bold">
+        {/* first line */}
+        <div className="flex items-center gap-[15px]">
+          <span>Discover</span>
+
+          <HeroFrame
+            height={{ base: 34, lg: 68 }}
+            width={{ base: 123, lg: 326 }}
+            bgImage="/images/hero-frame-1.jpg"
+            />
+
+          <span>Collect</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
