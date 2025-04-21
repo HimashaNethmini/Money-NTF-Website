2260f3fcc8937c0f5779d781aaa28e1f1910fb4b
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-21 22:31:25 +0530

    added animation for stats values

--- my-app/app/_components/hero-section/stats.tsx
+++ my-app/app/_components/hero-section/stats.tsx
@@ -1,11 +1,32 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 const stats = () => {
   return (
     <div className="flex items-center justify-center gap-[50px]">
-      <StatItem name={"Rare Nfts"} number={"10k+"} />
-      <StatItem name={"Products"} number={"70k+"} />
-      <StatItem name={"Clients"} number={"100k+"} />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+      >
+        <StatItem name={"Rare Nfts"} number={"10k+"} />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.4 }}
+      >
+        <StatItem name={"Products"} number={"70k+"} />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.6 }}
+      >
+        <StatItem name={"Clients"} number={"100k+"} />
+      </motion.div>
     </div>
   );
 };
