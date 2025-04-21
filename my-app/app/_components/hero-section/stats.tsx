d83ec9444f22e3154d89dbe3ea15258aba0033d1
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-21 22:24:53 +0530

    add stats

--- my-app/app/_components/hero-section/stats.tsx
+++ my-app/app/_components/hero-section/stats.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const stats = () => {
   return (
-    <div className='flex items-center justify-center'>
-      
+    <div className="flex items-center justify-center gap-[50px]">
+      <StatItem name={"Rare Nfts"} number={"10k+"} />
+      <StatItem name={"Products"} number={"70k+"} />
+      <StatItem name={"Clients"} number={"100k+"} />
     </div>
-  )
-}
+  );
+};
 
-export default stats
+const StatItem = ({ name, number }: { name: string; number: string }) => {
+  return (
+    <div className="flex flex-col items-center justify-center uppercase">
+      <span className="text-25 font-bold">{number}</span>
+      <span className="text-10">{name}</span>
+    </div>
+  );
+};
+
+export default stats;
