3bbd67cb6896e29f6f544756fe6131bbf92351d9
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-28 19:29:26 +0530

    add the heading with styles

--- my-app/app/_components/popular-collection-section/popular-collection-section.tsx
+++ my-app/app/_components/popular-collection-section/popular-collection-section.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import TextAnimation from '../text-animation'
 
 const PopularCollectionsSection = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-[29px]">
+
+    <span className="text-30 px-[15px] text-center font-bold">
+      <TextAnimation delay={0.2} divideBy='letter' >Get Popular Collection Here</TextAnimation>
+    </span>
+
+    {/* category section */}
     </div>
+
   )
 }
 
