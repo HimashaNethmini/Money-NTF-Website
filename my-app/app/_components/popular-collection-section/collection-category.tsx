fdce0cf917c458179230b57b2d5622b61cdee61c
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-28 20:15:24 +0530

    added conditions

--- my-app/app/_components/popular-collection-section/collection-category.tsx
+++ my-app/app/_components/popular-collection-section/collection-category.tsx
@@ -1,5 +1,6 @@
 import { CATEGORY_DATA } from "@/app/_data/category";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 const CollectionCategory = ({
   activeCategory,
@@ -9,12 +10,17 @@ const CollectionCategory = ({
   setActiveCategory: (category: string) => void; //function with a return type of void
 }) => {
   return (
-    
     <div className="flex flex-wrap items-center justify-center gap-[14px]">
       {CATEGORY_DATA.map((category) => (
         <div
           key={category.value}
-          className="cursor-pointer text-10 rounded-full border border-white/50 px-[20px] py-[18px] text-center"
+          
+          //include tailWind merge - if the selected category is the current category then the border bg is orange
+          className={twMerge(
+            "cursor-pointer text-10 rounded-full border border-white/50 px-[20px] py-[18px] text-center",
+
+            activeCategory === category.value && "bg-primary border-none"
+          )}
           onClick={() => setActiveCategory(category.value)}
         >
           {category.name}
