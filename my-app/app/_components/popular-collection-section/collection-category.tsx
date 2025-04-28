15971bc3d2f8c873c728cc2f5f9b7d219a4ddb09
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-28 19:36:56 +0530

    make the collection category basic styling and component

--- my-app/app/_components/popular-collection-section/collection-category.tsx
+++ my-app/app/_components/popular-collection-section/collection-category.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { CATEGORY_DATA } from "@/app/_data/category";
+import React from "react";
 
 const CollectionCategory = () => {
   return (
-    <div>
-      
+    <div className="flex flex-wrap items-center justify-center gap-[14px]">
+      {CATEGORY_DATA.map((category) => (
+        <div
+          key={category.value}
+          className="cursor-pointer text-10 rounded-full border border-white/50 px-[20px] py-[18px] text-center"
+        >
+          {category.name}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default CollectionCategory
+export default CollectionCategory;
