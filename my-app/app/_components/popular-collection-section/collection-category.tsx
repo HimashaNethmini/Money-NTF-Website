3ff8e06eaf6e02aa6b29a79b833bcec78b8b31b7
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-28 19:56:00 +0530

    checking the active collection category

--- my-app/app/_components/popular-collection-section/collection-category.tsx
+++ my-app/app/_components/popular-collection-section/collection-category.tsx
@@ -1,13 +1,21 @@
 import { CATEGORY_DATA } from "@/app/_data/category";
 import React from "react";
 
-const CollectionCategory = () => {
+const CollectionCategory = ({
+  activeCategory,
+  setActiveCategory,
+}: {
+  activeCategory: string;
+  setActiveCategory: (category: string) => void; //function with a return type of void
+}) => {
   return (
+    
     <div className="flex flex-wrap items-center justify-center gap-[14px]">
       {CATEGORY_DATA.map((category) => (
         <div
           key={category.value}
           className="cursor-pointer text-10 rounded-full border border-white/50 px-[20px] py-[18px] text-center"
+          onClick={() => setActiveCategory(category.value)}
         >
           {category.name}
         </div>
--- my-app/app/_components/popular-collection-section/popular-collection-section.tsx
+++ my-app/app/_components/popular-collection-section/popular-collection-section.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import TextAnimation from '../text-animation'
-import CollectionCategory from './collection-category'
+import React, { useState } from "react";
+import TextAnimation from "../text-animation";
+import CollectionCategory from "./collection-category";
 
 const PopularCollectionsSection = () => {
+
+  // checking the active collection category
+  const [activeCategory, setActiveCategory] = useState<string>("all");
+
   return (
     <div className="flex flex-col gap-[29px]">
+      <span className="text-30 px-[15px] text-center font-bold">
+        <TextAnimation delay={0.2} divideBy="letter">
+          Get Popular Collection Here
+        </TextAnimation>
+      </span>
 
-    <span className="text-30 px-[15px] text-center font-bold">
-      <TextAnimation delay={0.2} divideBy='letter' >Get Popular Collection Here</TextAnimation>
-    </span>
-
-    {/* category section */}
-    <CollectionCategory />
-
+      {/* category section - passing params */}
+      <CollectionCategory
+        activeCategory={activeCategory}
+        setActiveCategory={setActiveCategory}
+      />
     </div>
+  );
+};
 
-  )
-}
-
-export default PopularCollectionsSection
+export default PopularCollectionsSection;
