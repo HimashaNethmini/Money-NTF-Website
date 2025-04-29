24c4dd3b9e86c778a64f25d258a38fb3d67db0e9
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-29 18:49:04 +0530

    defining the card data types

--- my-app/app/_components/popular-collection-section/collection-card.tsx
+++ my-app/app/_components/popular-collection-section/collection-card.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const CollectionCard = () => {
+const CollectionCard = ({ card } : {
+  card: {
+    name: string;
+    username: string;
+    avatar: string;
+    category: string;
+    nfts: {
+      bg: string;
+      image: string;
+    } [];
+  };
+}) => {
   return (
     <div>
-      
+      {card.name}
     </div>
   )
 }
