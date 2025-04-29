204280b3856b1050035a8dc723805e80f2f7db79
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-29 19:21:02 +0530

    second column - completed

--- my-app/app/_components/popular-collection-section/collection-card.tsx
+++ my-app/app/_components/popular-collection-section/collection-card.tsx
@@ -16,18 +16,19 @@ const CollectionCard = ({
   };
 }) => {
   return (
-    <div
-      className="flex w-[100%] flex-col gap-[20px] rounded-[10px] bg-white/10 p-[10px]">
-
+    <div className="flex w-[100%] flex-col gap-[20px] rounded-[10px] bg-white/10 p-[10px]">
       {/* header */}
       <div className="flex items-start justify-between">
-
         {/* user info */}
         <div className="flex items-center gap-3">
           {/* user image */}
           <div className="relative h-[26px] w-[26px]">
-
-            <Image src={card.avatar} alt="user" fill className="rounded-full object-cover"/>
+            <Image
+              src={card.avatar}
+              alt="user"
+              fill
+              className="rounded-full object-cover"
+            />
           </div>
 
           <div className="flex flex-col">
@@ -35,17 +36,19 @@ const CollectionCard = ({
             <span className="text-12">{card.name}</span>
 
             {/* user username */}
-            <span className="text-10 text-white/50">
-              {card.username}
-            </span>
+            <span className="text-10 text-white/50">{card.username}</span>
           </div>
-
         </div>
 
         {/* likes count */}
         <div className="flex items-center gap-2 ">
           <div className="relative h-[18px] w-[18px]">
-            <Image src="/images/heart.png" alt="heart" fill className="object-contain" />
+            <Image
+              src="/images/heart.png"
+              alt="heart"
+              fill
+              className="object-contain"
+            />
           </div>
 
           <span className="text-[11px]">63</span>
@@ -57,7 +60,21 @@ const CollectionCard = ({
 
         {/* first columns with one image */}
         <div className="relative w-full h-[257px] overflow-hidden">
-          <Image src={card.nfts[0].image} alt="collection" fill className="object-cover" />
+          <Image
+            src={card.nfts[0].image}
+            alt="collection"
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        {/* second column */}
+        <div className="flex flex-col gap-4">
+          <div className="relative flex-1 w-full rounded-lg overflow-hidden">
+            <Image src={card.nfts[1].image} alt="colletion" fill  className="object-contain"/>
+          </div>
+
+          {/* third column  */}
         </div>
       </div>
     </div>
