99c37eb2fe74789554ce8c74eac6474aef61ffc9
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-29 19:08:01 +0530

    make the basic layout

--- my-app/app/_components/popular-collection-section/collection-card.tsx
+++ my-app/app/_components/popular-collection-section/collection-card.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-const CollectionCard = ({ card } : {
+const CollectionCard = ({
+  card,
+}: {
   card: {
     name: string;
     username: string;
@@ -9,14 +12,30 @@ const CollectionCard = ({ card } : {
     nfts: {
       bg: string;
       image: string;
-    } [];
+    }[];
   };
 }) => {
   return (
-    <div>
-      {card.name}
-    </div>
-  )
-}
+    <div
+      className="flex w-[100%] flex-col gap-[20px] rounded-[10px] bg-white/10 p-[10px]">
 
-export default CollectionCard
+      {/* header */}
+      <div className="flex items-start justify-between">
+
+        {/* user info */}
+        <div className="flex items-center gap-3">
+          {/* user image */}
+          <div className="relative h-[26px] w-[26px]">
+
+            <Image src={card.avatar} alt="user" fill />
+          </div>
+        </div>
+      </div>
+
+      {/* grid */}
+      <div>Grid</div>
+    </div>
+  );
+};
+
+export default CollectionCard;
