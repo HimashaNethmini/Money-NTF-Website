205d8eafcf4b1c3b9870499f070fc05f82026b7a
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-30 14:08:47 +0530

    make the companies-section layout

--- my-app/app/_components/companies-section/companies-section.tsx
+++ my-app/app/_components/companies-section/companies-section.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
 
 const CompaniesSection = () => {
   return (
-    <div>
+    <div
+        className='relative w-full py-10 overflow-hidden'>
+            <div className="whitespace-nowrap">
+                {
+                    Array.from({ length: 7 }).map((_, index) => (
+                        <div className="inline-block">
+                            <Image 
+                                src={`/images/companies/${index + 1}.png`}
+                                alt={`Company ${index + 1}`}
+                                height={150}
+                                width={150}
+                                className="min-w-[100px] w-[150px] h-[150px] object-contain"
+                            />
+                        </div>  
+                ))}
+            </div>
       
     </div>
   )
