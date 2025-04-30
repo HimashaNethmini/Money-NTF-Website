bc63033ab03b6937085fd6ce9a67376457efd2ae
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-30 13:58:38 +0530

    make it responsive - subscribe section

--- my-app/app/_components/subscribe-section/subscribe-section.tsx
+++ my-app/app/_components/subscribe-section/subscribe-section.tsx
@@ -5,15 +5,15 @@ import Button from '../button'
 const SubscribeSection = () => {
   return (
     <div 
-        className='flex flex-col w-full items-center justify-center gap-[16px]'>
+        className='flex flex-col w-full items-center justify-center gap-[16px] lg:gap-[37px]'>
       <span 
-        className='text-30 px-[45px] text-center font-bold'>
+        className='text-30 px-[45px] text-center font-bold lg:text-60'>
         <TextAnimation delay={0.5} divideBy='letter'>
             Subscribe to get latest updates
         </TextAnimation>
         </span>
 
-        <span className='text-[9px]'>
+        <span className='text-[9px] lg:text-14'>
             <TextAnimation delay={0.4} divideBy='word'>
                 Lorem ispum is simply the dummy text for printing and types.
             </TextAnimation>
@@ -25,7 +25,7 @@ const SubscribeSection = () => {
         {/* input */}
         <input type="email" 
             placeholder='Enter Email address' 
-            className='w-full max-w-[320px] min-w-[216px] rounded-md border-white/20 bg-white/10 px-[16px]' 
+            className='w-full max-w-[320px] min-w-[216px] rounded-md border-white/20 bg-white/10 px-[16px] lg:min-w-[554px]' 
         />
 
         {/* button */}
