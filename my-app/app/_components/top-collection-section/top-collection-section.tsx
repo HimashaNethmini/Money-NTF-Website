c5d52594cc6f86cba8331b147606d5313dc67551
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-30 12:13:01 +0530

    completed the page

--- my-app/app/_components/top-collection-section/top-collection-section.tsx
+++ my-app/app/_components/top-collection-section/top-collection-section.tsx
@@ -5,17 +5,42 @@ import { TOP_COLLECTION_DATA } from "@/app/_data/top-collection";
 
 const TopCollectionSection = () => {
   return (
-    <div className="flex flex-col gap-[16px]">
-      <span className="text-30 px-[28px] text-center font-bold">
+    <div 
+      className="flex flex-col gap-[16px]">
+      <span 
+        className="text-30 px-[28px] text-center font-bold">
         <TextAnimation delay={0.2}> Our Top Collection </TextAnimation>
       </span>
 
       {/* collection list */}
-      <div className="flex flex-col items-center justify-center gap-[10px]">
+      <div 
+        className="flex flex-col items-center justify-center gap-[10px]">
+        
         {TOP_COLLECTION_DATA.map((item, index) => (
-          <div key={index} className="">
-            <span className="text-16 font-bold">{index + 1} </span>
-            <Image src={item.image} alt="item_name" fill />
+          <div
+            key={index}
+            className="flex w-[100px] max-w-[320px] items-center gap-[8px] border-b-white/40 border-b py-[9px]"
+          >
+            <span 
+              className="text-16 font-bold">{index + 1}. </span>
+            
+            <Image
+              src={item.image}
+              alt="item_name"
+              width={35}
+              height={35}
+              className="rounded-full"
+            />
+
+            <div 
+              className="flex flex-col gap-[5px]">
+              <span 
+                className="text-[12px] font-bold">{item.name}</span>
+              <span 
+                className="font-bold text-10 text-white/40">
+                ${item.price.toLocaleString()}
+              </span>
+            </div>
           </div>
         ))}
       </div>
