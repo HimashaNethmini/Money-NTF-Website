1a3953004cf4178bbe6f2d4b961901dd966586c5
HimashaNethmini <himashanethmini05@gmail.com>
2025-04-30 13:31:41 +0530

    completed the subscribe button

--- my-app/app/_components/subscribe-section/subscribe-section.tsx
+++ my-app/app/_components/subscribe-section/subscribe-section.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import TextAnimation from '../text-animation'
+import Button from '../button'
 
 const SubscribeSection = () => {
   return (
     <div 
         className='flex flex-col w-full items-center justify-center gap-[16px]'>
       <span 
-        className='text-30 px-[28px] text-center font-bold'>
+        className='text-30 px-[45px] text-center font-bold'>
         <TextAnimation delay={0.5} divideBy='letter'>
             Subscribe to get latest updates
         </TextAnimation>
@@ -18,6 +19,20 @@ const SubscribeSection = () => {
             </TextAnimation>
         </span>
 
+     {/* input filed and button */}
+     <div className="flex gap-[9px]">
+
+        {/* input */}
+        <input type="email" 
+            placeholder='Enter Email address' 
+            className='w-full max-w-[320px] min-w-[216px] rounded-md border-white/20 bg-white/10 px-[16px]' 
+        />
+
+        {/* button */}
+        <Button variant="filled">
+            Subscribe
+        </Button>
+     </div>
     </div>
   )
 }
