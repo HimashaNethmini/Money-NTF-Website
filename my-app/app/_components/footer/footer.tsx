c2907948809c29404480c41b220d28f858004150
HimashaNethmini <himashanethmini05@gmail.com>
2025-05-01 13:11:28 +0530

    make the footer

---
+++ my-app/app/_components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-col gap-5'>
+
+        {/* logo and subscribe section */}
+        
+      
+    </div>
+  )
+}
+
+export default Footer
