9a298a769b8d57e4ab367f649728903219bb7ec0
HimashaNethmini <himashanethmini05@gmail.com>
2025-05-01 13:14:55 +0530

    logo and subscribe section- footer

--- my-app/app/_components/footer/footer.tsx
+++ my-app/app/_components/footer/footer.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Logo from '../header/logo'
+import Button from '../button'
 
 const Footer = () => {
   return (
     <div className='flex flex-col gap-5'>
 
         {/* logo and subscribe section */}
-        
+        <div className="flex items-center justify-between">
+            <Logo />
+
+            <div className="flex items-center gap-4">
+                <span className='font-bold text-nowrap'>
+                    Ready to collect?
+                </span>
+
+                <Button variant='filled'>Subscribe</Button>
+            </div>
+
+        </div>
+
       
     </div>
   )
