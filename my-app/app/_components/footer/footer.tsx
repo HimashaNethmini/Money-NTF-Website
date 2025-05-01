1d1d077e9157429a8919bc1323ceee6c9dbc3d98
HimashaNethmini <himashanethmini05@gmail.com>
2025-05-01 15:00:52 +0530

    complete the footer section

--- my-app/app/_components/footer/footer.tsx
+++ my-app/app/_components/footer/footer.tsx
@@ -5,14 +5,18 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className='flex flex-col gap-5'>
+    <div 
+        className='flex flex-col gap-5'>
 
         {/* logo and subscribe section */}
-        <div className="flex items-center justify-between lg:justify-around">
+        <div 
+            className="flex items-center justify-between lg:justify-around">
             <Logo />
 
-            <div className="flex items-center gap-4">
-                <span className='font-bold text-nowrap lg:text-25'>
+            <div 
+                className="flex items-center gap-4">
+                <span 
+                    className='font-bold text-nowrap lg:text-25'>
                     Ready to collect?
                 </span>
 
@@ -22,18 +26,24 @@ const Footer = () => {
         </div>
 
         {/* divider */}
-        <div className='h-[1px] bg-white/50 w-full' />
+        <div 
+            className='h-[1px] bg-white/50 w-full' />
 
         {/* bottom with links */}
-        <div className="flex flex-col gap-8 lg:flex-row lg:justify-between">
+        <div 
+            className="flex flex-col gap-8 lg:flex-row lg:justify-between">
 
             {/* page links - css grid into 3 cols*/}
-            <div className="grid grid-cols-3 gap-4 lg:order-2 lg:gap-10">
+            <div 
+                className="grid grid-cols-3 gap-4 lg:order-2 lg:gap-10">
 
                 {/* column 1 */}
-                <div className='flex flex-col gap-4 text-16 lg:text-20'>
-                    <span className='text-primary font-bold'>Marketplace</span>
-                    <div className="flex flex-col gap-2">
+                <div 
+                    className='flex flex-col gap-4 text-16 lg:text-20'>
+                    <span 
+                        className='text-primary font-bold'>Marketplace</span>
+                    <div 
+                        className="flex flex-col gap-2">
                         <Link href="/">Create</Link>
                         <Link href="/">Mining</Link>
                         <Link href="/">Selling</Link>
@@ -42,9 +52,12 @@ const Footer = () => {
                 </div>
                 
                     {/* column 2 */}
-                <div className='flex flex-col text-16 gap-4 lg:text-20'>
-                    <span className='text-primary font-bold'> Category</span>
-                    <div className='flex flex-col gap-2'>
+                <div 
+                    className='flex flex-col text-16 gap-4 lg:text-20'>
+                    <span 
+                        className='text-primary font-bold'> Category</span>
+                    <div 
+                        className='flex flex-col gap-2'>
                         <Link href="/">Create</Link>
                         <Link href="/">Mining</Link>
                         <Link href="/">Selling</Link>
@@ -64,7 +77,8 @@ const Footer = () => {
             </div>
 
             {/* two lines and social icons */}
-            <div className="flex flex-col items-center text-center gap-5 lg:items-start lg:text-left lg:justify-between">
+            <div 
+                className="flex flex-col items-center text-center gap-5 lg:items-start lg:text-left lg:justify-between">
                 <div>
                     <span className='text-16 lg:text-25'>
                         Create, sell and collect
@@ -76,7 +90,8 @@ const Footer = () => {
                 </div>
 
                 {/* social links */}
-                <div className=" flex items-center gap-4">
+                <div 
+                    className=" flex items-center gap-4">
                     <Link href="/">
                         <img src="/images/social/facebook.png" alt="facebook" width={40} height={40} />
                     </Link>
